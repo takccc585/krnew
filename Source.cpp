d805be7e10ecd09c845e665ebf2ecb691e002af2
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-10-09 18:20:34 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//1
+	/*int hp;
+	cin >> hp;
+	bool s = hp > 0;
+	cout << boolalpha << s;*/
+	//2
+	/*int temp;
+	cin >> temp;
+	if (temp>20) {
+		cout <<"on" ;
+	}
+	else {
+		cout << "off";
+	}*/
+	//3
+	/*int tempP,tempK,vl;
+	cin >> tempP >> tempK >> vl;
+	if ((tempP > tempK) and (vl<80)){
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+	//4
+	/*float y,x;
+	cin >> x;
+	if (x > 0) {
+		y = sqrt(x - 2);
+	}
+	else if (x==0){
+		y = 0;
+	}
+	else if (x < 0) {
+		y = abs(x);
+	}
+		cout << y;*/
+	//5
+	/*int pal;
+	cin >> pal;
+		switch (pal) {
+		case 1:cout << "bolshoy" << endl; break;
+		case 2:cout << "ukazatelniy" << endl; break;
+		case 3:cout << "sredniy" << endl; break;
+		case 4:cout << "bezumyannuy" << endl; break;
+		case 5:cout << "mizinec" << endl; break;
+		default:cout << "error" << endl; break;
+	}*/
+	//6
+	/*int tempK, tempU;
+	cin >> tempK >> tempU;
+	if (tempK > 20) {
+		cout << "ac on";
+	}
+	else if ((tempK > tempU) and (tempK < 20)){
+		cout << "open window";
+	}
+	else {
+		cout << "gg";
+	}*/
+	return 0;
+}
